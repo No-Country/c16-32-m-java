e2ff23f974cf7ee276039e32996eab5ad82f9572
Leandro Deferrari <leandro_deferrari@hotmail.com>
2024-02-12 16:39:05 -0300

    User Entity created

--- src/main/java/com/c1632mjava/c1632mjava/Domain/Entities/User.java
+++ src/main/java/com/c1632mjava/c1632mjava/Domain/Entities/User.java
@@ -1,4 +1,30 @@
 package com.c1632mjava.c1632mjava.Domain.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private LocalDate birthdate;
+    private String email;
+    private String password;
+    private String photo;
+    private Enum gender;
+    private String pronouns;
+    private String description;
+    private Enum socialBattery;
+    private String currentSong;
+    private boolean active;
 }
