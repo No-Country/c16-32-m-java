7ccad9fad8f9c55fc1da927c3ce891ece871c9d0
Leandro Deferrari <leandro_deferrari@hotmail.com>
2024-03-07 00:00:00 -0300

    Alberto Perez

--- tinder-spotify/src/Components/EditarPerfil/MusicaReciente/MusicaReciente.jsx
+++ tinder-spotify/src/Components/EditarPerfil/MusicaReciente/MusicaReciente.jsx
@@ -22,17 +22,17 @@ const MusicaReciente = () => {
       </div>
       <div className="contenedor-con-borde">
         <div className="musica-reciente">
-          {cancionesEscuchadas && 
-          cancionesEscuchadas.map((cancion, index) => (
-            <div key={index} className="cancion">
-              <button className="boton-cancion" 
-              onClick={() => agregarCancionEscuchada(cancion)}>
-                <img src={Spotify} alt={`Logo de la 
+          {cancionesEscuchadas &&
+            cancionesEscuchadas.map((cancion, index) => (
+              <div key={index} className="cancion">
+                <button className="boton-cancion"
+                  onClick={() => agregarCancionEscuchada(cancion)}>
+                  <img src={Spotify} alt={`Logo de la 
                 canción ${index + 1}`} />
-                <span>{cancion}</span>
-              </button>
-            </div>
-          ))}
+                  <span>{cancion}</span>
+                </button>
+              </div>
+            ))}
         </div>
       </div>
       {/*<CancionesEscuchadasLoader onCancionesCargadas={handleCancionesCargadas} />*/}
--- tinder-spotify/src/Components/HomePrivado/Persona2/PrincipalFoto2/PrincipalFoto2.jsx
+++ tinder-spotify/src/Components/HomePrivado/Persona2/PrincipalFoto2/PrincipalFoto2.jsx
@@ -9,19 +9,21 @@ import fondo5 from '../../../../assets/fondo5.png';
 import fondo6 from '../../../../assets/fondo6.png';
 import fondo7 from '../../../../assets/fondo7.png';
 import fondo8 from '../../../../assets/fondo8.png';
+import { useNavigate } from 'react-router-dom';
 
 const fotos = [fondo5, fondo6, fondo7, fondo8];
 
 const PrincipalFoto2 = ({ onNextClick, onPrevClick }) => {
   const [fotoActual, setFotoActual] = useState(0);
   const [mostrarFlechaIzquierda, setMostrarFlechaIzquierda] = useState(false);
-  
+  const navigate = useNavigate();
+
   const avanzarFoto = () => {
     setFotoActual((prevFoto) => {
       if (prevFoto + 1 < fotos.length) {
-        setMostrarFlechaIzquierda(true); 
+        setMostrarFlechaIzquierda(true);
       } else {
-        setMostrarFlechaIzquierda(false); 
+        setMostrarFlechaIzquierda(false);
       }
       return (prevFoto + 1) % fotos.length;
     });
@@ -30,9 +32,9 @@ const PrincipalFoto2 = ({ onNextClick, onPrevClick }) => {
   const retrocederFoto = () => {
     setFotoActual((prevFoto) => {
       if (prevFoto - 1 >= 0) {
-        setMostrarFlechaIzquierda(true); 
+        setMostrarFlechaIzquierda(true);
       } else {
-        setMostrarFlechaIzquierda(false); 
+        setMostrarFlechaIzquierda(false);
       }
       return (prevFoto - 1 + fotos.length) % fotos.length;
     });
@@ -40,6 +42,14 @@ const PrincipalFoto2 = ({ onNextClick, onPrevClick }) => {
 
   const handlePrevClick = () => {
     retrocederFoto();
+  };
+
+  const handleLinkChat = async () => {
+    try {
+      navigate('/chat');
+    } catch (error) {
+      setAlert('Hubo un error al entrar al chat. Por favor, inténtalo de nuevo.');
+    }
   };
 
   return (
@@ -55,12 +65,12 @@ const PrincipalFoto2 = ({ onNextClick, onPrevClick }) => {
         <img className="flecha" src={flecha} alt="Flecha dere" onClick={avanzarFoto} />
       </div>
       <div className="bot9">
-        <button className="atras"  onClick={onPrevClick}>
+        <button className="atras" onClick={onPrevClick}>
           <img src={fleIzqui} alt="Atrás" /> Atrás
         </button>
-        <button className="chat">Chat</button>
+        <button className="chat" onClick={handleLinkChat}>Chat</button>
         <button className="siguiente" onClick={onNextClick}>
-          Siguiente <img src={fledere} alt="Siguiente" />
+          <img src={fledere} alt="Siguiente" /> Siguiente
         </button>
       </div>
     </div>
